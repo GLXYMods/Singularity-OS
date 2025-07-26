5399650b5229bac7a0a3a5d55cb4b7f89274a16f
GLXYMods <gnmmodding123@gmail.com>
2025-07-26 04:25:06 -0400

    Update Buttons.cs

--- Buttons.cs
+++ Buttons.cs
@@ -17,10 +17,10 @@ namespace AetherTemp.Menu
         public static ButtonInfo[][] buttons = new ButtonInfo[][]
         {
             new ButtonInfo[] { // Main Mods
-                new ButtonInfo { buttonText = "News", method =() => Main.news(), disableMethod =() => Main.newsoff(), enabled = true, toolTip = "Opens the news page."},
+                new ButtonInfo { buttonText = "News", method =() => Main.news(), isTogglable = false, toolTip = "Opens the news page."},
                 new ButtonInfo { buttonText = "Modules", method =() => SettingsMods.Modules(), isTogglable = false, toolTip = "Opens the modules page."},
                 new ButtonInfo { buttonText = "Settings", method =() => SettingsMods.MainSettings(), isTogglable = false, toolTip = "Opens the settings page."},
-                new ButtonInfo { buttonText = "Info", method =() => Main.infoshit(), disableMethod =() => Main.offInfoShit(), toolTip = "Opens the settings page."},
+                new ButtonInfo { buttonText = "Info", method =() => Main.infoshit(), isTogglable = false, toolTip = "Opens the settings page."},
             },
 
             new ButtonInfo[] { // Main Settings
@@ -39,8 +39,11 @@ namespace AetherTemp.Menu
 
             new ButtonInfo[] { // Movement
                 new ButtonInfo { buttonText = "Platforms", method =() => Movement.platforms(), toolTip = "Walk with cubes."},
+                new ButtonInfo { buttonText = "Sticky Platforms", method =() => Movement.StickyPlats(), toolTip = "Walk with sticky cubes."},
+                new ButtonInfo { buttonText = "Inviz Platforms", method =() => Movement.Invizplatforms(), toolTip = "Walk with inviz cubes."},
+                new ButtonInfo { buttonText = "Noclip Platforms", method =() => Movement.NoclipPlats(), toolTip = "Walk with cubes and you noclip."},
                 new ButtonInfo { buttonText = "Fly", method =() => Movement.Fly(), toolTip = "Fly."},
-                new ButtonInfo { buttonText = "Trigger Fly", method =() => Movement.Fly(), toolTip = "Fly with trigger."},
+                new ButtonInfo { buttonText = "Trigger Fly", method =() => Movement.TriggerFly(), toolTip = "Fly with trigger."},
                 new ButtonInfo { buttonText = "Joystick Fly", method =() => Movement.JoystickFly(), toolTip = "Fly with joystick."},
                 new ButtonInfo { buttonText = "Fast Fly", method =() => Movement.FastFly(), toolTip = "Fly fast."},
                 new ButtonInfo { buttonText = "Slow Fly", method =() => Movement.FastFly(), toolTip = "Fly slow."},
@@ -60,20 +63,46 @@ namespace AetherTemp.Menu
                 new ButtonInfo { buttonText = "Throw Controller", method =() => Movement.ThrowControllers(), toolTip = "Throw Controllers."},
                 new ButtonInfo { overlapText = "Wall Walk Type:", buttonText = "Wall Walk Type:", method =() => Advantages.ChangeWallWalkType(), isTogglable = false, toolTip = "Change the assist type with wall walk."},
                 new ButtonInfo { buttonText = "Wall Walk", method =() => Advantages.WallWalk(), toolTip = "Have assist with walls."},
+                new ButtonInfo { buttonText = "Speed Boost", method =() => Movement.Speed(), toolTip = "Move Faster."},
+                new ButtonInfo { buttonText = "Mega Speed Boost", method =() => Movement.mega(), toolTip = "Move Very Fast."},
+                new ButtonInfo { buttonText = "Mosa Boost", method =() => Movement.mosa(), toolTip = "Move Slightly Faster."},
             },
 
-            new ButtonInfo[] { // Visual
+            new ButtonInfo[] { // Visual               
+                //new ButtonInfo { buttonText = "Players", method =() => SettingsMods.players(), isTogglable = false, toolTip = "Opens the player category"},
                 new ButtonInfo { buttonText = "Overview", method =() => OverView.Initialize(), disableMethod =() => OverView.DisableOverView(), enabled = true, toolTip = "See yourself when your vrrig is disabled."},
                 new ButtonInfo { buttonText = "Box Esp", method =() => Visual.BoxEsp(), toolTip = "See People Through Walls With A Box."},
                 new ButtonInfo { buttonText = "Hollow Box Esp", method =() => Visual.HollowBoxEsp(), toolTip = "See People Through Walls With A Hollow Box."},
+                new ButtonInfo { buttonText = "Wireframe Esp", method =() => Visual.wireframeEsp(), toolTip = "Wire frame esp so you can see people through walls."},
                 new ButtonInfo { buttonText = "Sphere Esp", method =() => Visual.sphereEsp(), toolTip = "See People Through Walls With A Sphere."},
                 new ButtonInfo { buttonText = "Head Esp", method =() => Visual.headEsp(), toolTip = "See People Through Walls With A Sphere And Line To Their Head."},
+                new ButtonInfo { buttonText = "Hand Esp", method =() => Visual.HandESP(), toolTip = "See People Through Walls With A Sphere To Their Hand."},
                 new ButtonInfo { buttonText = "NameTag", method =() => Visual.NameTags(), toolTip = "See Peoples Names Above Their Head."},
                 new ButtonInfo { buttonText = "Tracers", method =() => Visual.tracers(), toolTip = "See Peoples Through Walls With A Line."},
                 new ButtonInfo { buttonText = "Chams", method =() => Visual.Chams(), disableMethod =() => Visual.ChamsOff(), toolTip = "See people with a outline through walls."},
             },
 
-            new ButtonInfo[] { // Networked
+            new ButtonInfo[] { // projectiles
+                new ButtonInfo { overlapText = "Change Projectile Color", buttonText = "Change Projectile Color", method =() => Projectiles.change(), isTogglable = false, toolTip = "CS Projectile Spamer Change"},
+                new ButtonInfo { overlapText = "Change Trail", buttonText = "Change Trail", method =() => Projectiles.trail(), isTogglable = false, toolTip = "CS Projectile Spamer Change"},
+                new ButtonInfo { buttonText = "Water Ballon Spam[CS]", method =() => Projectiles.sig(), toolTip = "Projectile Spam[CS]!"},
+                new ButtonInfo { buttonText = "Snowball Spam[CS]", method =() => Projectiles.sig2(), toolTip = "Projectile Spam[CS]!"},
+                new ButtonInfo { buttonText = "Ice Spam[CS]", method =() => Projectiles.sig3(), toolTip = "Projectile Spam[CS]!"},
+                new ButtonInfo { buttonText = "Heart Spam[CS]", method =() => Projectiles.sig4(), toolTip = "Projectile Spam[CS]!"},
+                new ButtonInfo { buttonText = "Paintball Spam[CS]", method =() => Projectiles.sig5(), toolTip = "Projectile Spam[CS]!"},
+                new ButtonInfo { buttonText = "Leaf Spam[CS]", method =() => Projectiles.sig6(), toolTip = "Projectile Spam[CS]!"},
+                new ButtonInfo { buttonText = "Waterballon Gun[CS]", method =() => Projectiles.waterballongun(), toolTip = "Shoot Waterballoons[CS]!"},
+                new ButtonInfo { buttonText = "Waterballon Gun Ran[CS]", method =() => Projectiles.waterballongunrandom(), toolTip = "Shoot Waterballoons[CS]!"},
+                new ButtonInfo { buttonText = "Snowball Gun[CS]", method =() => Projectiles.snowballgun(), toolTip = "Shoot Snowballs[CS]!"},
+                new ButtonInfo { buttonText = "Heart Gun[CS]", method =() => Projectiles.heartgun(), toolTip = "Shoot hearts[CS]!"},
+                new ButtonInfo { buttonText = "Ice Gun[CS]", method =() => Projectiles.rainbowgun(), toolTip = "Shoot Ice[CS]!"},
+                new ButtonInfo { buttonText = "Paintball Gun[CS]", method =() => Projectiles.paintballgun(), toolTip = "Shoot paintballs[CS]!"},
+                new ButtonInfo { buttonText = "Leaf Gun[CS]", method =() => Projectiles.leafgun(), toolTip = "Shoot leafs[CS]!"},
+                new ButtonInfo { buttonText = "Deadshot Gun[CS]", method =() => Projectiles.deadshotgun(), toolTip = "Shoot paintballs[CS]!"},
+                new ButtonInfo { buttonText = "Piss[CS]", method =() => Projectiles.Piss(), toolTip = "Shoot Piss[CS]!"},
+                new ButtonInfo { buttonText = "Cum[CS]", method =() => Projectiles.Cum(), toolTip = "Shoot Cum[CS]!"},
+                new ButtonInfo { buttonText = "Throw Up[CS]", method =() => Projectiles.Throw(), toolTip = "Throw up[CS]!"},
+                new ButtonInfo { buttonText = "Rain[CS]", method =() => Projectiles.Rain(), toolTip = "Throw up[CS]!"},
             },
 
             new ButtonInfo[] { // Overpowered
@@ -85,6 +114,16 @@ namespace AetherTemp.Menu
                 new ButtonInfo { buttonText = "Auto Juke", method =() => Movement.AutoJuke(), toolTip = "Auto Juke."},
                 new ButtonInfo { buttonText = "Ghost On Touch", method =() => Overpowered.GhostOnTouch(), toolTip = "Whenever someone touches your head you go into ghost monke."},
                 new ButtonInfo { buttonText = "Inviz On Touch", method =() => Overpowered.InvizOnTouch(), toolTip = "Whenever someone touches your head you go into inviz monke."},
+                new ButtonInfo { buttonText = "AI Movement [<color=red>BETA</color>]", method =() => AIMovementShit.Initiate(), toolTip = "Become An AI."},
+                new ButtonInfo { buttonText = "Rail Gun", method =() => Fun.SexGun(), toolTip = "Rail the player you shoot."},
+                new ButtonInfo { buttonText = "Rail Random", method =() => Fun.SexRandom(), toolTip = "Rail a random player."},
+                new ButtonInfo { buttonText = "Get Railed Gun", method =() => Fun.GetSexGun(), toolTip = "Get railed by the person you shoot."},
+                new ButtonInfo { buttonText = "Rail Face Gun", method =() => Fun.GetHeadGun(), toolTip = "Rail the players face you shoot."},
+                new ButtonInfo { buttonText = "Rail Random Face", method =() => Fun.GetHeadRandom(), toolTip = "Rail a random players face."},
+                new ButtonInfo { buttonText = "Give Head Gun", method =() => Fun.HeadGun(), toolTip = "Gives head to the person you shoot."},
+                new ButtonInfo { buttonText = "Give Head Random", method =() => Fun.HeadRandom(), toolTip = "Gives head to a random person."},
+                new ButtonInfo { buttonText = "Kiss Gun", method =() => Fun.KissGun(), toolTip = "Kiss the player you shoot."},
+                new ButtonInfo { buttonText = "Spoof Player", method =() => Overpowered.SpoofPlayer(), isTogglable = false, toolTip = "Spoofs your color and name."},
             },
 
 
@@ -99,13 +138,16 @@ namespace AetherTemp.Menu
                 new ButtonInfo { buttonText = "Save Mods", method =() => Main.Save(), isTogglable = false, toolTip = "Save your enabled mods."},
                 new ButtonInfo { buttonText = "Load Mods", method =() => Main.Load(), isTogglable = false, toolTip = "Load your saved mods."},
                 new ButtonInfo { buttonText = "Save Settings", method =() => Main.SaveSettings(), isTogglable = false, toolTip = "Saves your settings, AUTO LOADS WHEN YOU START UP YOUR GAME!"},
+                new ButtonInfo { buttonText = "Menu Animations", method =() => Main.menuAnimations(true), disableMethod =() => Main.menuAnimations(false), toolTip = "Enables menu animations!"},
                 new ButtonInfo { overlapText = "Menu Theme", buttonText = "Menu Theme", method =() => mods.ChangeMenuTheme(), isTogglable = false, toolTip = "Cycle through different menu themes."},
                 new ButtonInfo { overlapText = "Esp Color", buttonText = "Esp Color", method =() => Visual.ChangeEspColor(), isTogglable = false, toolTip = "Cycle through different esp colors."},
                 new ButtonInfo { overlapText = "Btn Sound", buttonText = "Btn Sound", method =() => mods.changeBtnSound(), isTogglable = false, toolTip = "Cycle through different btn sounds."},
                 new ButtonInfo { buttonText = "Right/Left Hand", enableMethod =() => SettingsMods.RightHand(), disableMethod =() => SettingsMods.LeftHand(), toolTip = "Puts the menu on your right hand."},
                 new ButtonInfo { buttonText = "Disconnect Button", enableMethod =() => SettingsMods.EnableDisconnectButton(), disableMethod =() => SettingsMods.DisableDisconnectButton(), enabled = true, toolTip = "Toggles the disconnect button."},
                 new ButtonInfo { buttonText = $"Delete Time: {(Main.num == 2f ? "Default" : Main.num == 5f ? "Long" : "Fast")}", method = () => { Main.MenuDeleteTime(); foreach (var category in Buttons.buttons) foreach (var button in category) if (button.buttonText.StartsWith("Delete Time")) button.buttonText = $"Delete Time: {(Main.num == 2f ? "Default" : Main.num == 5f ? "Long" : "Fast")}"; }, isTogglable = false, toolTip = "Changes menu delete time." },                
-                new ButtonInfo { buttonText = "Boards", method =() => Visual.boards(), isTogglable = false, enabled = true, toolTip = "Enable boards, YOU CANT TURN IT OFF :)" },                
+                new ButtonInfo { buttonText = "Boards", method =() => Visual.boards(), isTogglable = false, enabled = true, toolTip = "Enable boards, YOU CANT TURN IT OFF :)" },
+                new ButtonInfo { buttonText = "Hear Self", method =() => Soundboard.hearself(true), disableMethod =() => Soundboard.hearself(false), toolTip = "Makes you be able to hear yourself."},
+                new ButtonInfo { buttonText = "Arraylist", method =() => Main.Arraylst(true), disableMethod =() => Main.Arraylst(false), enabled = true, toolTip = "Toggle arraylist."},
             },
 
             new ButtonInfo[] { // Notifications
@@ -123,6 +165,8 @@ namespace AetherTemp.Menu
             },
 
             new ButtonInfo[] { // Fun
+                new ButtonInfo { buttonText = "Projectiles", method =() => SettingsMods.projectiles(), isTogglable = false },
+                new ButtonInfo { buttonText = "Soundboard", method =() => SettingsMods.soundboard(), isTogglable = false },
                 new ButtonInfo { buttonText = "RC Bug", method =() => Fun.RCcar()},
                 new ButtonInfo { buttonText = "Voice Recognition", method =() => Voice.StartVoice(), isTogglable = false },
                 new ButtonInfo { buttonText = "Voice Recognition <color=red>[OFF]</color>", method =() => Voice.StopVoice(), isTogglable = false },
@@ -132,6 +176,10 @@ namespace AetherTemp.Menu
                 new ButtonInfo { buttonText = "Hands In Head", method =() => Fun.HandsINhead(), toolTip = "Hands In Head."},
                 new ButtonInfo { buttonText = "Kick All [<color=red>FAKE</color>]", method =() => Fun.kickAll(), disableMethod =() => Fun.disable(), toolTip = "Fake Kick Everyone."},
                 new ButtonInfo { buttonText = "Kick Gun [<color=red>FAKE</color>]", method =() => Fun.FakeKickGun(), toolTip = "Fake Kick The Player You Shoot."},
+                new ButtonInfo { buttonText = "Water [<color=red>LG</color>]", method =() => Fun.WaterL(), toolTip = "Spray water with your left hand."},
+                new ButtonInfo { buttonText = "Water [<color=red>RG</color>]", method =() => Fun.WaterR(), toolTip = "Spray water with your right hand."},
+                new ButtonInfo { buttonText = "Water Gun", method =() => Fun.WaterGun(), toolTip = "Water goes where you shoot."},
+                new ButtonInfo { buttonText = "Give Water Gun", method =() => Fun.GiveWaterGun(), toolTip = "Whoever You Shoot It Makes It Look Like They Are Modding."},
             },
 
             new ButtonInfo[] { // Rig
@@ -146,7 +194,13 @@ namespace AetherTemp.Menu
                 new ButtonInfo { buttonText = "Spin Slow <color=red>[RP]</color>", method =() => Rig.ItemStandThing(), toolTip = "Just spin slow."},
             },
 
+            new ButtonInfo[] { // Soundboard
+                
+            },
 
+            new ButtonInfo[] { // Player Shat
+                
+            },
 
             //always keep this at the bottom if you add another tab (by going to categories) make sure you put that section above this one:
 
@@ -156,8 +210,6 @@ namespace AetherTemp.Menu
            
             new ButtonInfo[] {
                 new ButtonInfo { buttonText = "Home", method =() => Global.ReturnHome(), isTogglable = false, toolTip = "Opens the home page."},
-                new ButtonInfo { buttonText = "nextpage", method =() => Main.NextPage(), isTogglable = true, enabled = true, toolTip = "Opens the home for the menu."},
-                new ButtonInfo { buttonText = "lastpage", method =() => Main.PreviousPage(), isTogglable = true, enabled = true, toolTip = "Opens the home for the menu."},
             },
 
         };
